1a2f7c1d503d2fd34bf65fb76a70082a7efe87d1
alexjachna <57778785+alexjachna@users.noreply.github.com>
2023-01-23 23:55:06 -0500

    Added new concept/design for About-me Page

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ hamburger.onclick = function() {
     navBar = document.querySelector("#nav-bar");
     navBar.classList.toggle("active");
 }
+
+var tabLinks = document.getElementsByClassName("tab-links");
+var tabContents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabName) {
+    for (tabLink of tabLinks) {
+        tabLink.classList.remove('active-link');
+    }
+
+    for (tabContent of tabContents) {
+        tabContent.classList.remove('active-tab');
+    }
+
+    event.currentTarget.classList.add('active-link');
+    document.getElementById(tabName).classList.add('active-tab');
+}
