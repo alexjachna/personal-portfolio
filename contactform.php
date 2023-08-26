bdf441c7c67d1aa4ca504c50928dfff7ec74afd1
Alex Jachna <57778785+alexjachna@users.noreply.github.com>
2023-08-25 21:02:25 -0400

    Added new project and contact form changes

--- contactform.php
+++ contactform.php
@@ -2,14 +2,16 @@
 
 if (isset($_POST['submit'])) {
     $name = $_POST['name'];
-    $mailFrom = $_POST['email'];
+    $email = $_POST['email'];
     $subject = $_POST['subject'];
     $message = $_POST['message'];
 
-    $mailTo = "alex.jachna@mail.utoronto.ca"; // Write domain email here
-    $headers = "From: ".$mailFrom;
-    $txt = "You have received an email from ".$name.".\n\n".$message;
+    $mailTo = "contact@alexjachna.com";
+    $headers = "From: ".$email;
+    $txt = "You have received an e-mail from ".$name.".\n\n".$message
 
     mail($mailTo, $subject, $txt, $headers);
-    header("Location: index.html");
+    header("Location: index.html?mailsend");
 }
+
+?>
--- script.js
+++ script.js
@@ -2,47 +2,44 @@
 hamburger = document.querySelector("#hamburger-menu");
 exitButton = document.querySelector("#exit-button");
 
-hamburger.onclick = function() {
-    navBar = document.querySelector("#nav-bar");
-    navBar.classList.toggle("active");
-}
+hamburger.onclick = function () {
+  navBar = document.querySelector("#nav-bar");
+  navBar.classList.toggle("active");
+};
 
-exitButton.onclick = function() {
-    navBar = document.querySelector("#nav-bar");
-    navBar.classList.toggle("active");
-}
-
+exitButton.onclick = function () {
+  navBar = document.querySelector("#nav-bar");
+  navBar.classList.toggle("active");
+};
 
 // About me navigation
 var tabLinks = document.getElementsByClassName("tab-links");
 var tabContents = document.getElementsByClassName("tab-contents");
 
 function opentab(tabName) {
-    for (tabLink of tabLinks) {
-        tabLink.classList.remove('active-link');
-    }
+  for (tabLink of tabLinks) {
+    tabLink.classList.remove("active-link");
+  }
 
-    for (tabContent of tabContents) {
-        tabContent.classList.remove('active-tab');
-    }
+  for (tabContent of tabContents) {
+    tabContent.classList.remove("active-tab");
+  }
 
-    event.currentTarget.classList.add('active-link');
-    document.getElementById(tabName).classList.add('active-tab');
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabName).classList.add("active-tab");
 }
-
 
 // Animation
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        }
-        else {
-            entry.target.classList.remove('show');
-        }
-    });
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
 });
 
-const hiddenElements = document.querySelectorAll('.hidden');
+const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
